2919241967a0002df05add24eeee61b96d01e1ba
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2023-11-09 18:14:10 -0600

    Added CSS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tip-Calculator-Project
+
+ This project is a simple Tip Calculator using Html, CSS, and Javascript.
--- main.js
+++ main.js
@@ -2,16 +2,11 @@ document.querySelector('button').addEventListener('click',tipCalculation)
 
 function tipCalculation(){
 
-const billCost = parseInt(document.querySelector(".billAmount").value);
-const decimalTipPercentage = parseInt(document.querySelector(".tipAmount").value) / 100;
+const billCost = parseInt(document.querySelector("#billAmount").value);
+const decimalTipPercentage = parseInt(document.querySelector("#tipAmount").value) / 100;
 const tipAmount = decimalTipPercentage * billCost;
 const totalBillCost =  tipAmount + billCost;
 
 document.querySelector('.totalAmount').innerText = totalBillCost;
-
-
-
-
-
 
 }
